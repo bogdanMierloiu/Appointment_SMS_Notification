ea51711059d268181a9c0d8535040ba4ca66c02e
bogdanMierloiu <bogdy.musiclover@gmail.com>
2023-05-21 22:44:37 +0300

    first commit

---
+++ sms_notification_app/src/test/java/com/bogdancode/sms_notification_app/SmsNotificationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bogdancode.sms_notification_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsNotificationAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
